85ce6149cfa2f0c8e3235ccc97cd1a7eb536ab88
thearhansheth <arhan.sheth@gmail.com>
2025-03-30 20:32:53 -0400

    Added tests for type error and full ring

--- HW/HW2 Codebase/boxing/tests/test_ring_model.py
+++ HW/HW2 Codebase/boxing/tests/test_ring_model.py
@@ -7,3 +7,15 @@ def test_enter_ring_success():
     boxer = Boxer(1, "Ali", 160, 70, 75.0, 28)
     ring.enter_ring(boxer)
     assert len(ring.ring) == 1
+
+def test_enter_ring_type_error():
+    ring = RingModel()
+    with pytest.raises(TypeError):
+        ring.enter_ring("NotABoxer")
+
+def test_enter_ring_full():
+    ring = RingModel()
+    ring.enter_ring(Boxer(1, "A", 160, 70, 75.0, 28))
+    ring.enter_ring(Boxer(2, "B", 160, 70, 75.0, 28))
+    with pytest.raises(ValueError):
+        ring.enter_ring(Boxer(3, "C", 160, 70, 75.0, 28))

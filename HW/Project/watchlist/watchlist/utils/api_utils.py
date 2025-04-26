f6fe33dbcbc6e58b9dca136a524d237eb62063bd
Pard3sh <pard3sh@bu.edu>
2025-04-26 16:39:45 -0400

    added api helper functions

--- HW/Project/watchlist/watchlist/utils/api_utils.py
+++ HW/Project/watchlist/watchlist/utils/api_utils.py
@@ -4,7 +4,7 @@ import requests
 
 #PLAYLIST VERSION // NOT EDITED
 
-from watchlist.utils.logger import configure_logger
+from utils.logger import configure_logger
 
 
 RANDOM_ORG_BASE_URL = os.getenv("RANDOM_ORG_BASE_URL",
@@ -60,3 +60,62 @@ def get_random(max: int) -> int:
     except requests.exceptions.RequestException as e:
         logger.error(f"Request to random.org failed: {e}")
         raise RuntimeError(f"Request to random.org failed: {e}")
+
+
+def validate_string(value) -> bool:
+    """
+    Ensures that the passed in variable is a string.
+
+    Args:
+        value (any): Value that will be checked to see if it is a string or not
+
+    Returns:
+        bool: Whether or not the value was a string or not.
+
+    """
+    if isinstance(value, str):
+        return True
+    else:
+        return False
+
+
+def api_get_movie_by_title(title: str):
+    """
+    Queries the TMDB for a movie by title.
+
+    Args:
+        title (str): The title of the movie.
+
+    Returns:
+        raw_movie_data (any): A dictionary that stores the movie information.
+
+    Raises:
+        ValueError: If the title is not inputted as a str or if the movie is not found in the TMDB.
+    """
+    url = "https://api.themoviedb.org/3/search/movie"
+
+    if validate_string(title):
+        query = title
+    else:
+        raise ValueError("Movie title must be inputted as a String!")
+
+    params = {
+        "query": query,
+        "page": 1,
+    }
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGIyMWM3NGFlZjNjNGFjZjlhNTI2YzMxNjgwZjI3MCIsIm5iZiI6MTc0NTUzNzE5Ni41OTM5OTk5LCJzdWIiOiI2ODBhYzhhY2YzY2Y2ZDY2ZDc5ZDJiNzYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.gv0w_3Y-CIfzbmu3KP1CnmcZYlTCHKR_nRfi_GJGcjM",
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+    data = response.json()
+    num_responses = data["total_results"]
+    raw_movie_data = None
+    if num_responses > 1:
+        raw_movie_data = data["results"][0]
+    else:
+        raise ValueError("Movie with entered title does not exist in the database. ")
+    return raw_movie_data
+

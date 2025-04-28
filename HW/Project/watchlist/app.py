d4b70c0bcec054b430fc7d4817aa93961ce09185
Pard3sh <pard3sh@bu.edu>
2025-04-27 22:40:39 -0400

    fixed route syntax

--- HW/Project/watchlist/app.py
+++ HW/Project/watchlist/app.py
@@ -568,7 +568,7 @@ def create_app(config_class=ProductionConfig) -> Flask:
                 500,
             )
 
-    @app.route("/api/get-movie-from-watchlist-by-title/<str:title>", methods=["GET"])
+    @app.route("/api/get-movie-from-watchlist-by-title/<title>", methods=["GET"])
     @login_required
     def get_movie_by_title(title: str) -> Response:
         """Retrieve a song from the playlist by its title.
